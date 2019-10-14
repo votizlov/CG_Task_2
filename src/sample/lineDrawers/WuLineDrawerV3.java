d3a05d55ee1c9c74f2cfa5b99df056c05ee06665
Votizlov <seriousfox@yandex.ru>
2019-10-14 10:40:38 +0300

    wu color antialiasing added x5

--- src/sample/lineDrawers/WuLineDrawerV3.java
+++ src/sample/lineDrawers/WuLineDrawerV3.java
@@ -14,10 +14,12 @@ public class WuLineDrawerV3 implements LineDrawer {
     public void drawLine(int x1, int y1, int x2, int y2, Color color) {
         DDALineDrawer lineDrawer = new DDALineDrawer(pixelDrawer);
         lineDrawer.drawLine(x1,  y1, x2,  y2, color);
-        lineDrawer.drawLine(x1-1,  y1, x2-1,  y2, color.darker());
-        lineDrawer.drawLine(x1+1,  y1, x2+1,  y2, color.darker());
-        lineDrawer.drawLine(x1, y1+1, x2, y2+1, color.darker());
-        lineDrawer.drawLine(x1,  y1-1, x2,  y2-1, color.darker());
+        int d = 1;
+        Color color1 = new Color(Color.HSBtoRGB(0, 1, 0.3f));
+        //lineDrawer.drawLine(x1-d,  y1+d, x2-d,  y2+d, color1);
+        lineDrawer.drawLine(x1,  y1-d, x2,  y2-d, color1);
+        lineDrawer.drawLine(x1, y1+d, x2, y2+d, color1);
+        //lineDrawer.drawLine(x1-d,  y1-d, x2-d,  y2-d, color1);
     }
 
     @Override
